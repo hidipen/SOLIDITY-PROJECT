c5b48451b3101cb8e0794aac9e52a48745877ff6
hidipen <dipenpaul754@gmail.com>
2025-09-09 03:01:02 +0530

    count number

---
+++ Counter.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract Counter{
+    uint256 public count; //getter function created
+
+    // // function to get current count
+    // function get() public view returns(uint256){
+    //     return count;
+    // }
+    //   function to increment count by 1
+    function inc() public {
+        count += 1;
+    }
+    // function to decrement count by 1
+    function dec() public {
+        count -= 1;
+    }
+
+}
